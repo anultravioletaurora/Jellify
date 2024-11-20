fe0ffc393d692d2f76b2d744d637719966e5cc2b
Violet Caulfield <violet@cosmonautical.cloud>
2024-11-20 06:20:15 -0600

    fix build

--- api/queries.ts
+++ api/queries.ts
@@ -1,12 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { QueryKeys } from "../enums/query-keys";
-import { createPublicApi } from "./queries/functions/api";
 import { buildApiClient } from "./client";
-
-export const usePublicApi = () => useQuery({
-    queryKey: [QueryKeys.PublicApi],
-    queryFn: createPublicApi
-});
 
 export const useApi = (serverUrl?: string, username?: string, password?: string, accessToken?: string) => useQuery({
     queryKey: [QueryKeys.Api, serverUrl, username, password, accessToken],
